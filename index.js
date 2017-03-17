db815794b7835d7fa700059c53c3448ce919c897
Jon Gold <hello@designedbygold.com>
2017-03-17 11:41:54 -0700

    reader should just pass env variables

--- index.js
+++ index.js
@@ -21,26 +21,31 @@ const unblock = client => user_id =>
     client.post('blocks/destroy', { user_id }, done)
   ).mapRej(err => new Error(`error on ${user_id} ${err.message}`));
 
-// stabilizeFutures = Array (Future a b) -> Array (Future Left Right)
+// stabilizeFutures :: Array (Future a b) -> Array (Future Left Right)
 const stabilizeFutures = xs =>
   xs.map(Future.fold(Either.Left, Either.Right));
 
+// makeClient :: env -> TwitterClient
+const makeClient = env =>
+  Future.try(() => new Twit(env));
+
 // main :: int -> Reader Env -> (Future Error [Response])
 const main = batchSize => Reader(env =>
-  getBlocks(env.client)
-    .map(res => res.ids.map(id => unblock(env.client)(id)))
-    .map(stabilizeFutures)
-    .chain(reqs => Future.parallel(batchSize, reqs))
+  makeClient(env)
+  .chain(client =>
+    getBlocks(client)
+      .map(res => res.ids.map(id => unblock(client)(id)))
+      .map(stabilizeFutures)
+      .chain(reqs => Future.parallel(batchSize, reqs))
+  )
 );
 
 if (require.main === module) {
   main(5).run({
-    client: new Twit({
-      consumer_key: process.env.CONSUMER_KEY,
-      consumer_secret: process.env.CONSUMER_SECRET,
-      access_token: process.env.ACCESS_TOKEN,
-      access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    }),
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   }).fork(logError, log);
 }
 
